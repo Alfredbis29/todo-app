155ba4970c620997c3b5f34b267c39fbaf89c814
alfredbis29 <kalumunabisimwa5@gmail.com>
2025-07-06 18:40:25 -0400

    fix bugs

---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,36 @@
+import { FC } from 'react';
+
+interface Todo {
+  id: number;
+  text: string;
+  completed: boolean;
+}
+
+interface Props {
+  todo: Todo;
+  toggleComplete: (id: number) => void;
+  removeTodo: (id: number) => void;
+}
+
+const TodoItem: FC<Props> = ({ todo, toggleComplete, removeTodo }) => {
+  return (
+    <li className="flex items-center justify-between bg-gray-100 p-3 rounded-lg">
+      <div
+        className={`flex-1 cursor-pointer ${
+          todo.completed ? 'line-through text-gray-400' : ''
+        }`}
+        onClick={() => toggleComplete(todo.id)}
+      >
+        {todo.text}
+      </div>
+      <button
+        onClick={() => removeTodo(todo.id)}
+        className="ml-3 text-red-500 hover:text-red-700 transition"
+      >
+        ✕
+      </button>
+    </li>
+  );
+};
+
+export default TodoItem;
